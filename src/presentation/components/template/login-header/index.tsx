89d205cc8a2022c1ea8358b8294fdfe18dcf3920
Gabriel4420 <gabriel_rodrigues_perez@hotmail.com>
2022-07-16 14:53:28 -0300

    refactor: :truck: move input to a separete file

--- src/presentation/components/template/login-header/index.tsx
+++ src/presentation/components/template/login-header/index.tsx
@@ -1,10 +1,11 @@
-import Logo from '@/presentation/components/logo'
+import Logo from '@/presentation/components/atoms/logo'
 import Styles from './styles.scss'
 import React, { memo } from 'react'
 
 const LoginHeader: React.FC = () => {
   return (
     <header className={Styles.header}>
+      <Logo />
       <h1>4Dev - Enquetes para programadores</h1>
     </header>
   )
--- src/presentation/pages/login/index.tsx
+++ src/presentation/pages/login/index.tsx
@@ -1,8 +1,9 @@
-import Spinner from '@/presentation/components/spinner'
-import Header from '@/presentation/components/login-header'
-import Footer from '@/presentation/components/Footer'
+import Spinner from '@/presentation/components/atoms/spinner'
+import Header from '@/presentation/components/template/login-header'
+import Footer from '@/presentation/components/template/Footer'
 import Styles from './styles.scss'
 import React from 'react'
+import Input from '@/presentation/components/mols/input'
 
 const Login: React.FC = () => {
   return (
@@ -10,18 +11,11 @@ const Login: React.FC = () => {
       <Header />
       <form className={Styles.form} action="">
         <h2>Login</h2>
-        <div className={Styles.inputWrap}>
-          <input type="email" name="email" placeholder="Digite seu email" />
-          <span className={Styles.status}>🔴</span>
-        </div>
-        <div className={Styles.inputWrap}>
-          <input
-            type="password"
-            name="password"
-            placeholder="Digite sua senha"
-          />
-          <span className={Styles.status}>🔴</span>
-        </div>
+
+        <Input type="email" name="email" placeholder="Digite seu email" />
+
+        <Input type="password" name="password" placeholder="Digite sua senha" />
+
         <button className={Styles.submit} type="submit">
           Entrar
         </button>
