d6a5449f721dd389ab13092022b32f68516f4996
Gabriel4420 <gabriel_rodrigues_perez@hotmail.com>
2022-07-16 21:42:58 -0300

    test: :white_check_mark: ensure Login starts with submit button disabled

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -3,9 +3,11 @@ import { Login } from '@/presentation/pages'
 import React from 'react'
 
 describe('Login Component', () => {
-  test('Should not render spinner and error on start ', () => {
+  test('Should start with initial state ', () => {
     const { getByTestId } = render(<Login />)
     const errorWrap = getByTestId('error-wrap')
     expect(errorWrap.childElementCount).toBe(0)
+    const submitButton = getByTestId('submit') as HTMLButtonElement
+    expect(submitButton.disabled).toBe(true)
   })
 })
