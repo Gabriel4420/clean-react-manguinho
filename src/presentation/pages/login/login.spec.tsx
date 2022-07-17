025844721ee6c41ea842330a2a078ae475f0baed
Gabriel4420 <gabriel_rodrigues_perez@hotmail.com>
2022-07-17 13:57:54 -0300

    refactor: :zap: change Login tests to use faker

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -6,6 +6,7 @@ import {
   fireEvent,
   cleanup,
 } from '@testing-library/react'
+import faker from 'faker'
 import React from 'react'
 
 type SutTypes = {
@@ -50,21 +51,27 @@ describe('Login Component', () => {
     const { sut, validationSpy } = makeSut()
 
     const emailInput = sut.getByTestId('email')
-    fireEvent.input(emailInput, { target: { value: 'any_email' } })
+
+    const email = faker.internet.email()
+
+    fireEvent.input(emailInput, { target: { value: email } })
 
     expect(validationSpy.fieldName).toBe('email')
 
-    expect(validationSpy.fieldValue).toBe('any_email')
+    expect(validationSpy.fieldValue).toBe(email)
   })
 
   test('Should call validation with correct password ', () => {
     const { sut, validationSpy } = makeSut()
 
     const passwordInput = sut.getByTestId('password')
-    fireEvent.input(passwordInput, { target: { value: 'any_pass' } })
+
+    const password = faker.internet.password()
+
+    fireEvent.input(passwordInput, { target: { value: password } })
 
     expect(validationSpy.fieldName).toBe('password')
 
-    expect(validationSpy.fieldValue).toBe('any_pass')
+    expect(validationSpy.fieldValue).toBe(password)
   })
 })
