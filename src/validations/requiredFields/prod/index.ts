1a54370b5de20c7989ae6c945589ada7b936aaf7
Gabriel4420 <gabriel_rodrigues_perez@hotmail.com>
2022-07-21 11:00:30 -0300

    feat: :sparkles: ensure RequiredFieldValidation returns falsy if field is not empty

--- src/validations/requiredFields/prod/index.ts
+++ src/validations/requiredFields/prod/index.ts
@@ -1,10 +1,9 @@
-import { FieldValidation } from "@/validations/protocols";
-import { RequiredFieldError } from "@/validations/errors";
-
+import { FieldValidation } from '@/validations/protocols'
+import { RequiredFieldError } from '@/validations/errors'
 
 export class RequiredFieldValidation implements FieldValidation {
   constructor(readonly field: string) {}
   validate(value: string): Error {
-    return new RequiredFieldError()
+    return value ? null : new RequiredFieldError()
   }
 }
