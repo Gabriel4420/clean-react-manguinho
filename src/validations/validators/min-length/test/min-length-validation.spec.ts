763ffba8649f9fc67e2fbc83932640ef730c3499
Gabriel4420 <gabriel_rodrigues_perez@hotmail.com>
2022-07-22 12:43:28 -0300

    test: :white_check_mark: ensure MinLengthValidation returns falsy if value is valid

--- src/validations/validators/min-length/test/min-length-validation.spec.ts
+++ src/validations/validators/min-length/test/min-length-validation.spec.ts
@@ -3,8 +3,26 @@ import { MinLengthValidation } from '@/validations/validators'
 
 describe('MinLengthValidation', () => {
   test('Should return error if value is invalid', () => {
+    //
+
     const sut = new MinLengthValidation('field', 5)
+
     const error = sut.validate('123')
+
     expect(error).toEqual(new InvalidFieldError())
+
+    //
+  })
+
+  test('Should return falsy if value is valid', () => {
+    //
+
+    const sut = new MinLengthValidation('field', 5)
+
+    const error = sut.validate('12345')
+
+    expect(error).toBeFalsy()
+
+    //
   })
 })
