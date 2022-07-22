aabda22095e09d40d6359581681ef36f9aeff6af
Gabriel4420 <gabriel_rodrigues_perez@hotmail.com>
2022-07-21 23:14:03 -0300

    test: :white_check_mark: ensure EmailValidation returns falsy if email is valid

--- src/validations/validators/email/test/email-validation.spec.ts
+++ src/validations/validators/email/test/email-validation.spec.ts
@@ -1,12 +1,21 @@
-import { InvalidFieldError } from "@/validations/errors"
-import { EmailValidation } from "@/validations/validators/email"
+import { InvalidFieldError } from '@/validations/errors'
+import { EmailValidation } from '@/validations/validators/email'
+import faker from 'faker'
 
 describe('EmailValidation', () => {
   test('Should return error if email is invalid', () => {
     const sut = new EmailValidation('email')
 
-    const error = sut.validate('')
+    const error = sut.validate(faker.random.word())
 
     expect(error).toEqual(new InvalidFieldError())
+  })
+
+  test('Should return falsy if email is valid', () => {
+    const sut = new EmailValidation('email')
+
+    const error = sut.validate(faker.internet.email())
+
+    expect(error).toBeFalsy()
   })
 })
