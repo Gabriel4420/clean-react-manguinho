dd8ffe54564cf6a3dc6247d5deaed4b4cf5a44ac
Gabriel4420 <gabriel_rodrigues_perez@hotmail.com>
2022-07-24 09:16:57 -0300

    test: :white_check_mark: ensure ValidationBuilder has a email helper

--- src/validations/builder/test/validation-builder.spec.ts
+++ src/validations/builder/test/validation-builder.spec.ts
@@ -1,9 +1,15 @@
-import { RequiredFieldValidation, ValidationBuilder as sut } from '@/validations'
+import { EmailValidation, RequiredFieldValidation, ValidationBuilder as sut } from '@/validations'
 
 describe('ValidationBuilder', () => {
   test('Should return RequiredField validation ', () => {
     const validations = sut.field('any_field').required().build()
 
     expect(validations).toEqual([new RequiredFieldValidation('any_field')])
+  })
+
+  test('Should return EmailValidation ', () => {
+    const validations = sut.field('any_field').email().build()
+
+    expect(validations).toEqual([new EmailValidation('any_field')])
   })
 })
