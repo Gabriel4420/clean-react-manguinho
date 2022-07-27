b52119c75d7c706c54c6a5e3d1f98135a977b332
Gabriel4420 <gabriel_rodrigues_perez@hotmail.com>
2022-07-27 15:16:51 -0300

    refactor: :recycle: change api url factory to receive path by param

--- src/main/factories/usecases/remote/authentication/index.ts
+++ src/main/factories/usecases/remote/authentication/index.ts
@@ -4,5 +4,5 @@ import { Authentication } from '@/domain'
 import { RemoteAuthentication } from '@/data'
 
 export const makeRemoteAuthentication = (): Authentication => {
-  return new RemoteAuthentication(makeApiUrl(), makeAxiosHttpClient())
+  return new RemoteAuthentication(makeApiUrl('/login'), makeAxiosHttpClient())
 }
